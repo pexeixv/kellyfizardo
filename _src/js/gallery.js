637a091826fca6dea38654c331b35a1207940bde
Gavin Pereira <68476117+pexeixv@users.noreply.github.com>
2021-11-10 19:29:03 +0530

    gallery filters added

--- _src/js/gallery.js
+++ _src/js/gallery.js
@@ -2,3 +2,22 @@ var colc = new Colcade('.gallery_grid', {
     columns: '.gallery_column',
     items: '.gallery_photo'
 })
+
+var images = document.querySelectorAll('.gallery_photo')
+
+var filterGallery = (arg, el) => {
+    document.querySelector('.gallery_filter-active').classList.remove('gallery_filter-active')
+    el.classList.add('gallery_filter-active')
+    if (arg === 'all') {
+        images.forEach(image => image.classList.remove('hide'))
+        return 0
+    }
+    images.forEach(image => {
+        if (!image.classList.contains(`gallery_photo-${arg}`))
+            image.classList.add('hide')
+        else
+            image.classList.remove('hide')
+    })
+
+
+}
